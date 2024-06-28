8a87dd93a0d424af32d597db17e0f0be585b8ae5
tanishqmalik <tanishqvideo@gmail.com>
2024-06-28 13:52:58 +0530

    okay

--- fullStackBasic/frontend/src/App.jsx
+++ fullStackBasic/frontend/src/App.jsx
@@ -5,30 +5,31 @@ import './App.css'
 import axios from 'axios'
 
 function App() {
-  const [Jokes , setJokes]= useState([])
+  const [jokes , setJokes]= useState([])
+
 
   useEffect(()=>{
     axios.get('/api/jokes')
-    .then((response)=>{
+    .then ((response)=>{
       setJokes(response.data)
     })
     .catch((error)=>{
-      console.log(error);
+      console.log(error)
     })
   })
 
   return (
     <>
     <h1>Your Jokes</h1>
-    <p>Jokes : {Jokes.length}</p>
+    <p>Jokes : {jokes.length}</p>
 
     {
-      Jokes.map((joke, index)=>{
+      jokes.map((joke, index)=>(
         <div key={joke.id}>
           <h3>{joke.title}</h3>
           <p>{joke.content}</p>
         </div>
-      })
+      ))
     }
     </>
   )
--- fullStackBasic/frontend/vite.config.js
+++ fullStackBasic/frontend/vite.config.js
@@ -4,8 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   server:{
-    poxy:{
-      '/api':'http://localhost:3000',
+    proxy:{
+      '/api': 'http://localhost:3000',
     }
   },
   plugins: [react()],
